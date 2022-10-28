b0f0662b1c406229607e9ebbb4ff7f6bdae8966d
ViableGolem4376 <90400959+ViableGolem4376@users.noreply.github.com>
2022-10-28 13:21:05 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "grades.h"
+#include "grades.cpp"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    Student s;
+    s.calculateGrade();
+};
